4603a4809ec42d4aa58f4a3b93946615f037b1bf
June Kang <Jkang34@gmail.com>
2020-11-18 08:40:24 -0500

    count sentences lab

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    complex_sent = self.split(/[.?!]+/)
+    return complex_sent.count
   end
 end
